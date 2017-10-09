6853f832378594835225c9d7ec096c3e3a778fa6
You Hye In <yty9323@hanamil.net>
2017-10-09 14:51:50 +0900

    //make utils, make commands

--- src/commands.c
+++ src/commands.c
@@ -1,4 +1,7 @@
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 
 #include "commands.h"
 
@@ -6,25 +9,31 @@ int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
 
-  // TODO: Fill it!
-  return 0;
+  else
+    return chdir(argv[1]);
 }
 
 int do_pwd(int argc, char** argv) {
   if (!validate_pwd_argv(argc, argv))
     return -1;
 
-  // TODO: Fill it!
+  char *cmd;
+  if((cmd = getcwd(NULL,8096))!=0){
+    printf("%s\n",cmd);
+    return 0;
+  }
+  else return -1;
+  }
 
-  return 0;
-}
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+  if(strcmp(argv[0],"cd")==0&&argc==2)
+	  return 1;
+  else return 0;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+  if(strcmp(argv[0],"pwd")==0&&argc ==1)
+	  return 1;
+  else return 0;
 }
--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,32 @@
 #include "utils.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+   int count = 0;
+   char *cmdLineCopy = (char*)malloc(2048);
+   strcpy(cmdLineCopy, command);
+   char* tok = strtok(cmdLineCopy, "' '\t\n");
+   (*argv) = (char**) malloc(sizeof(char*)*1000);
+   (*argv)[count]=(char*)malloc(sizeof(char)*1000);
+
+   if(tok==NULL){
+	   *argc=1;
+	   strcpy((*argv)[0],"");
+	   return;
+   
+   }
+
+   while(tok !=NULL){
+	   strcpy((*argv)[count], tok);
+	   tok = strtok(NULL,"' ',\t\n");
+	   count++;
+	   (*argv)[count]=(char*)malloc(sizeof(char)*1000);
+}
+
+*argc= count;
+free(cmdLineCopy);
 }
